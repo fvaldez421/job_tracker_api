57a131caa80c89233122ea96e8ee30c53e4d5971
Frank Valdez <fvaldez421@gmail.com>
2021-03-23 21:13:03 -0700

    added sample request and db architecture

--- app/__init__.py
+++ app/__init__.py
@@ -1,34 +1,36 @@
 import os
-
+from dotenv import load_dotenv
 from flask import Flask, request
 # from flask_mongoengine import MongoEngine
-
 from app.database.db import initialize_db
 from app.routes.jobs import jobs_bp
 
-def create_app(test_config=None):
+# this will generate a path to this file (<os abs path>/job_tracker_api/app), we use replace to get the project root
+# HACK! we should clean this up ^^^^
+basedir = os.path.abspath(os.path.dirname(__file__)).replace('/app', '')
+env_path = os.path.join(basedir, '.env')
+load_dotenv(env_path)
+
+def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping()
+    app.config['MONGODB_SETTINGS'] = {
+        'host': os.environ.get('MONGODB_HOST', 'mongodb://127.0.0.1/job_tracker'),
+        # the username and password will be required for staging and prod environments
+        # 'username': os.environ.get('MONGODB_USERNAME', ''),
+        # 'password': os.environ.get('MONGODB_PASSWORD', ''),
+    }
     
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-    
+    # initialize mongo db
     initialize_db(app)
+    
+    # register app routes
     app.register_blueprint(jobs_bp)
+
     # a simple page that says hello
     @app.route('/')
     def noPath():
         return 'Hello, no path!!!!!!'
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
 
     return app
 
