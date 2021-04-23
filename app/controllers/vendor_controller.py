3c1ffd63a9a1f68422f34ac0c1360ae5f1f8f0fd
Frank Valdez <fvaldez421@gmail.com>
2021-04-22 19:33:02 -0700

    feat: added model abstraction for documents with created and modifying users, them implemented on vendors

--- app/controllers/vendor_controller.py
+++ app/controllers/vendor_controller.py
@@ -1,5 +1,4 @@
 from app.database.models import Vendor
-from app.database.models import User
 
 
 class VendorController:
@@ -13,29 +12,38 @@ class VendorController:
         return vendor
 
     @staticmethod
-    def find_by_name(query_name):
+    def find_by_name(query_name, specialValue=None):
         return Vendor.objects(name__icontains=query_name.strip())
 
     @staticmethod
-    def create_vendor(vendor_data=None, user_id=None):
+    def create_vendor(vendor_data=None):
         vendor = None
         if 'name' in vendor_data:
             vendor_name = vendor_data['name'].strip()
+            mod_id = vendor_data.get('mod_id')
             existing_vendor = Vendor.objects(name__iexact=vendor_name)
             if not existing_vendor:
-                vendor = Vendor(name=vendor_name)
-                vendor = vendor.save() 
+                vendor = Vendor(
+                    name=vendor_name,
+                    created_by=mod_id
+                )
+                vendor = vendor.save()
         return vendor
-        
+
     @staticmethod
     def update_vendor(vendor_id=None, updates=None):
         vendor = None
         success = False
         if vendor_id != None or updates != None:
-            vendor = VendorController.find_by_id(vendor_id=vendor_id)
-            if vendor != None:
-                if 'name' in updates:
-                    vendor.name = updates['name'].strip()
+            mod_id = updates.get('mod_id')
+            vendor_name = updates.get('name')
+            if vendor_name or mod_id:
+                vendor = VendorController.find_by_id(vendor_id=vendor_id)
+                if vendor != None:
+                    if vendor_name:
+                        vendor.name = vendor_name.strip()
+                    if mod_id:
+                        vendor.modified_by = mod_id
                     vendor.save()
                     success = True
         return vendor if success else None
--- app/database/models.py
+++ app/database/models.py
@@ -2,38 +2,64 @@ import datetime
 from .db import db
 
 
-class User(db.Document):
+class DocumentWithDate(db.Document):
+    meta = {"allow_inheritance": True}
+    creation_date = db.DateTimeField()
+    modified_date = db.DateTimeField(default=datetime.datetime.now)
+
+    def set_date(self, *args, **kwargs):
+        if not self.creation_date:
+            self.creation_date = datetime.datetime.now()
+        self.modified_date = datetime.datetime.now()
+
+    def save(self, *args, **kwargs):
+        self.set_date(self, *args, **kwargs)
+        return super(db.Document, self).save(*args, **kwargs)
+
+
+class DocumentWithUser(db.Document):
+    meta = {"allow_inheritance": True}
+    created_by = db.StringField()
+    modified_by = db.StringField()
+
+    def set_user(self, *args, **kwargs):
+        created_by = None
+        modified_by = None
+        if hasattr(self, 'created_by'):
+            created_by = self.created_by
+        if hasattr(self, 'modified_by'):
+            modified_by = self.modified_by
+        if created_by != None or modified_by != None:
+            if not self.created_by and created_by:
+                self.created_by = created_by
+            self.modified_by = created_by if modified_by == None else modified_by
+
+    def save(self, *args, **kwargs):
+        self.set_user(self, *args, **kwargs)
+        return super(db.Document, self).save(*args, **kwargs)
+
+
+class DocumentWithDateUser(DocumentWithDate, DocumentWithUser):
+    def save(self, *args, **kwargs):
+        if hasattr(self, 'set_date'):
+            self.set_date(self, *args, **kwargs)
+        if hasattr(self, 'set_user'):
+            self.set_user(self, *args, **kwargs)
+        return super(db.Document, self).save(*args, **kwargs)
+
+
+class User(DocumentWithDate):
     name = db.StringField()
     email = db.EmailField()
-    creation_date = db.DateTimeField()
-    modified_date = db.DateTimeField(default=datetime.datetime.now)
-    def save(self, *args, **kwargs):
-        if not self.creation_date:
-            self.creation_date = datetime.datetime.now()
-        self.modified_date = datetime.datetime.now()
-        return super(User, self).save(*args, **kwargs)
 
-class Vendor(db.Document):
-    name = db.StringField()
-    creation_date = db.DateTimeField()
-    modified_date = db.DateTimeField(default=datetime.datetime.now)
-    def save(self, *args, **kwargs):
-        if not self.creation_date:
-            self.creation_date = datetime.datetime.now()
-        self.modified_date = datetime.datetime.now()
-        return super(Vendor, self).save(*args, **kwargs)
 
-class Job(db.Document):
-    #add user id
+class Vendor(DocumentWithDateUser):
     name = db.StringField()
-    #job_address = db.StringField()
-    #job_number = db.IntegerField()
+
+
+class Job(DocumentWithDateUser):
+    # add user id
+    name = db.StringField()
+    #address = db.StringField()
+    #number = db.IntegerField()
     #contract_amount = db.FloatField()
-    creation_date = db.DateTimeField()
-    modified_date = db.DateTimeField(default=datetime.datetime.now)
-    def save(self, *args, **kwargs):
-        if not self.creation_date:
-            self.creation_date = datetime.datetime.now()
-        self.modified_date = datetime.datetime.now()
-        return super(Vendor, self).save(*args, **kwargs)
-
