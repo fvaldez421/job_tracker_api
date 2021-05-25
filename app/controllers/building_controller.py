1d5db47c9627088be49e955b2a05b4cf19a9dca1
Jesus V <heyzeus.1096@gmail.com>
2021-05-24 23:14:04 -0700

    Added error messages to Building and General Contractor controllers

--- app/controllers/building_controller.py
+++ app/controllers/building_controller.py
@@ -1,5 +1,16 @@
 from app.database.models import Building
+from app.helpers.controller_helpers import ControllerHelpers
+from app.helpers.date_helpers import DateHelpers
 
+
+building_create_fields = [
+    'name',
+    'job',
+]
+building_update_fields = [
+    'name',
+    'job',
+]
 
 class BuildingController:
     @staticmethod
@@ -18,48 +29,75 @@ class BuildingController:
     @staticmethod
     def create_building(building_data=None):
         building = None
-        if 'name' in building_data and 'job' in building_data:
+        message = '"name" and "job" fields are required'
+        valid_data = ControllerHelpers.checkForAllUpdates(building_create_fields, building_data)
+
+        if valid_data:
             building_name = building_data['name'].strip()
             job = building_data.get('job')
             mod_id = building_data.get('mod_id')
             existing_building = Building.objects(name__iexact=building_name)
-            if not existing_building:
+            if existing_building:
+                message = 'a building with that name and job "{}", "{}" already exists.'.format(building_name, job)
+            else:
                 building = Building(
                     name=building_name,
+                    job=job,
                     created_by=mod_id
                 )
                 building = building.save()
-        return building
+                message = 'successfully created building'
+        return {
+            'value': building,
+            'success': True if building else False,
+            'message': message
+        }
 
     @staticmethod
     def update_building(building_id=None, updates=None):
         building = None
+        message = 'can not update buidling with empty request body'
         success = False
-        if building_id != None or updates != None:
+        has_updates = ControllerHelpers.checkForAnyUpdates(building_update_fields, updates)
+        if building_id != None and has_updates != None:
             mod_id = updates.get('mod_id')
             job = building.get('job')
             building_name = updates.get('name')
             if building_name or mod_id:
                 building = BuildingController.find_by_id(building_id=building_id)
                 if building != None:
+                    message = 'building with id "{}" not found'
+                else:
                     if building_name:
                         building.name = building_name.strip()
+                    if job:
+                        buidling.job = building_job.strip()
                     if mod_id:
                         building.modified_by = mod_id
                     building.save()
                     success = True
-        return building if success else None
+                    message = 'successfully updated building'
+        return {
+            'value': buidling,
+            'success': success,
+            'message': message
+        }
 
     @staticmethod
     def delete_building(building_id=None):
         building = None
         success = False
+        message = 'building with id "{}" not found'.format(building_id)
         if building_id != None:
             building = BuildingController.find_by_id(building_id=building_id)
             if building != None:
                 building.delete()
                 success = True
-        return success
+                message = 'successfully deleted building'
+        return {
+            'success': success,
+            'message': message
+        }
 
     @staticmethod
     def delete_all_buildings():
--- app/controllers/gencon_controller.py
+++ app/controllers/gencon_controller.py
@@ -1,5 +1,13 @@
 from app.database.models import GeneralContractor
+from app.helpers.controller_helpers import ControllerHelpers
+from app.helpers.date_helpers import DateHelpers
 
+gencon_create_fields = [
+    'name'
+]
+gencon_update_fields = [
+    'name'
+]
 
 class GeneralContractorController:
     @staticmethod
@@ -18,46 +26,68 @@ class GeneralContractorController:
     @staticmethod
     def create_generalcontractor(generalcontractor_data=None):
         generalcontractor = None
-        if 'name' in generalcontractor_data:
+        message = '"name" is a required field'
+        valid_data = ControllerHelpers.checkForAllUpdates(gencon_create_fields, generalcontractor_data)
+        if valid_data:
             generalcontractor_name = generalcontractor_data['name'].strip()
             mod_id = generalcontractor_data.get('mod_id')
             existing_generalcontractor = GeneralContractor.objects(name__iexact=generalcontractor_name)
-            if not existing_generalcontractor:
+            if existing_generalcontractor:
+                message = 'a general contractor with that name "{}" already exists'.format()
+            else:
                 generalcontractor = GeneralContractor(
                     name=generalcontractor_name,
                     created_by=mod_id
                 )
                 generalcontractor = generalcontractor.save()
-        return generalcontractor
+                message = 'successfully created general contractor'
+        return {
+            'value': generalcontractor,
+            'success': True if generalcontractor else False,
+            'message': message
+        }
 
     @staticmethod
     def update_generalcontractor(generalcontractor_id=None, updates=None):
         generalcontractor = None
         success = False
+        message = 'can not update general contractor with empty body'
+        has_updates = ControllerHelpers.checkForAnyUpdates(gencon_update_fields, updates)
         if generalcontractor_id != None or updates != None:
             mod_id = updates.get('mod_id')
             generalcontractor_name = updates.get('name')
             if generalcontractor_name or mod_id:
                 generalcontractor = GeneralContractorController.find_by_id(generalcontractor_id=generalcontractor_id)
                 if generalcontractor != None:
+                    message = 'general contractor with id "{}" not found'.format(generalcontractor_id)
                     if generalcontractor_name:
                         generalcontractor.name = generalcontractor_name.strip()
                     if mod_id:
                         generalcontractor.modified_by = mod_id
                     generalcontractor.save()
                     success = True
-        return generalcontractor if success else None
+                    message = 'successfully updated general contractor'
+        return {
+            'value': generalcontractor,
+            'success': success,
+            'message': message
+        }
 
     @staticmethod
     def delete_generalcontractor(generalcontractor_id=None):
         generalcontractor = None
         success = False
+        message = 'general contractor with id "{}" not found'.format(generalcontractor_id)
         if generalcontractor_id != None:
             generalcontractor = GeneralContractorController.find_by_id(generalcontractor_id=generalcontractor_id)
             if generalcontractor != None:
                 generalcontractor.delete()
                 success = True
-        return success
+                message = 'successfully deleted general contractor'
+        return {
+            'success': success,
+            'message': message
+        }
 
     @staticmethod
     def delete_all_generalcontractors():
